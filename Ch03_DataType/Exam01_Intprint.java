0b64200e34fb5c6f9180b1b4b754ba22c6400d99
snowcuphea <snowcuphea@gmail.com>
2019-12-08 19:15:05 +0900

    Init

---
+++ Ch03_DataType/Exam01_Intprint.java
@@ -0,0 +1,12 @@
+package Ch03_DataType;
+
+public class Exam01_Intprint {
+	
+public static void main(String[] args) {
+		
+		long num = 9409152000000L;
+		System.out.println("나의 주민번호 : " + num);
+
+}
+
+}
---
+++ Ch03_DataType/Exam02_ASCIIprint.java
@@ -0,0 +1,24 @@
+package Ch03_DataType;
+
+public class Exam02_ASCIIprint {
+
+	public static void main(String[] args) {
+	
+			String s1 = "1";
+			String s2 = "2";
+			boolean bxn = false;
+			char c1 = 'A';
+			char c2 = 'B';
+			char c3 = '1';
+			char c4 = '2';
+			int inx = 2;
+			
+			System.out.println("ASCII 값 'A' -> " + (int)c1);
+			System.out.println("ASCII 값 'B' -> " + (int)c2);
+			System.out.println("ASCII 값 '1' -> " + (int)c3);
+			System.out.println("ASCII 값 '2' -> " + (int)c4);
+			System.out.println( (char)inx );
+
+	}
+
+}
